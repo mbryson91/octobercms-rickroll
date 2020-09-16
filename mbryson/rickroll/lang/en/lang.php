da290232458850a6921022b20563e68e15cc6b6f
Matthew Bryson <matthew@mbryson.co.uk>
2020-09-16 10:22:49 +0100

    initial plugin setup

---
+++ mbryson/rickroll/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'RickRoll',
+        'description' => 'Rick roll those hacker bastards'
+    ]
+];
---
+++ mbryson/rickroll/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::redirect('/wp-admin', 'https://youtu.be/dQw4w9WgXcQ', 301);
+Route::redirect('/.env', 'https://youtu.be/dQw4w9WgXcQ', 301);
+Route::redirect('//.env', 'https://youtu.be/dQw4w9WgXcQ', 301);
+Route::redirect('/admin_login.php', 'https://youtu.be/dQw4w9WgXcQ', 301);
+Route::redirect('/wp-admin/admin-ajax.php', 'https://youtu.be/dQw4w9WgXcQ', 301);
